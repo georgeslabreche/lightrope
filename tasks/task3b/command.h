ec7bbfab99340cd3f057e77557be597741938db7
Georges Labrèche <georges@opendatakosovo.org>
2017-09-19 19:59:14 +0200

    Added comments.

--- tasks/task3b/command.h
+++ tasks/task3b/command.h
@@ -1,3 +1,14 @@
+/*
+ * Initialise the command object.
+ */
 void initCom(void);
+
+/*
+ * Write the command into a shared memory variable.
+ */
 void writeCom(unsigned char command);
+
+/*
+ * Read the command from a shared variable.
+ */
 unsigned char readCom(void);
