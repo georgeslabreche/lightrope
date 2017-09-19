bef64ac74b2e7125fbd760c63b108f140f6fe34d
Georges Labrèche <georges@opendatakosovo.org>
2017-09-19 21:17:00 +0200

    Commented main.c

--- tasks/task3b/main.c
+++ tasks/task3b/main.c
@@ -1,27 +1,50 @@
+/*
+ * Title: Assignment 2 - Tasks in FreeRTOS 
+ *
+ * Reason:
+ * 	- To familiarize with the task concept.
+ *	- To familiarize with the FreeRTOS.
+ *	- To familiarize with the STK500 development board.
+ *	- To prepare for the final RTOS system lab.
+ *
+ * Authors:
+ * 	Georges L. J. Labreche <geolab-7@student.ltu.se>
+ *	Natalie Lawton <natlaw-7@student.ltu.se>
+ */
 #include <avr/io.h>
 #include <FreeRTOS.h>
+
+/* Access to command related methods. */
 #include "command.h"
+
+/* Access to lightrope related methods. */
 #include "lightrope.h"
+
+/* Access to telecommand related methods. */
 #include "telecommand.h"
 
-
-void initPins(){
-	DDRB  = 0xFF; // Initialize PORTB to output
-	DDRD  = 0x00; // Initialize PORTD as input
-    PORTD = 0xFF; // Activate pull-ups
-	PORTB = 0b11111110;
+/*
+ * Initalise pins and ports that will be used.
+ */
+static void initPins(){
+	DDRB  = 0xFF; /* Initialise PORTB to output. */
+	DDRD  = 0x00; /* Initialise PORTD as input. */
+    	PORTD = 0xFF; /* Activate pull-ups. */
+	PORTB = 0b11111110; 
 }
 
-
+/*
+ * Main progran execution block.
+ */
 int main(void)
 {
-	initPins();
-	initInterrupt();
-	initCom();
-	initLightrope();
-	initTC();
+	initPins(); /* Initialise pins. */
+	initInterrupt(); /* Initialise interrupt in telecommand object. */
+	initCom(); /* Initialise command object. */
+	initLightrope(); /* Initialise lightrope object. */
+	initTC(); /* Initialise telecommand. */
+	
+	vTaskStartScheduler(); /* Start the scheduler to manage tasks. */
 
-    vTaskStartScheduler();
-
-    return 0;
+	return 0;
 }
