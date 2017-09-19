46b42d2f2f51eb720ec107740d1e3f96c027315a
Georges Labrèche <georges@opendatakosovo.org>
2017-09-19 21:32:32 +0200

    Commented telecommand object logic.

--- tasks/task3b/telecommand.c
+++ tasks/task3b/telecommand.c
@@ -1,3 +1,17 @@
+/*
+ * Title:
+ * 	The telecommand object.
+ *
+ * Description:
+ *	This object relays messages between the command
+ *	object and the lightrope object. It uses interrupts
+ *	to dispatch a message that is then saved in a shared
+ *	memory variable in the command object.
+ *
+ * Authors:
+ * 	Georges L. J. Labreche <geolab-7@student.ltu.se>
+ * 	Natalie Lawton <natlaw-7@student.ltu.se>
+ */
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <FreeRTOS.h>
@@ -6,57 +20,88 @@
 #include "command.h"
 #include "telecommand.h"
 
+/* The message queue used to relay commands. */
 xQueueHandle xQueue = NULL;
 
+/*
+ * The telecommand task method.
+ * Dequeues commands from the queue and writes them
+ * into the shared memory variable in the command object.
+ * Commands are enqueued into the queue from the interrupt
+ * service routine.
+ */
 static void listenTC()
-{
+{	
+	/* 
+	 * Save the time the task was put active
+	 * for the last time in this variable.
+	 */
+    	portTickType xLastWakeTime;
 	
-	// Save the time the task was put active
-    // for the last time in this variable
-    portTickType xLastWakeTime;
+	/* 
+	 * Initialize the variable once.
+	 * It will be updated automatically.
+	 */
+	xLastWakeTime = xTaskGetTickCount();
 
-    // Initialize the variable once
-    // It will be updated automatically
-    xLastWakeTime = xTaskGetTickCount();
-
+	/*
+	 * The command value will be read from the queue
+	 * and assigned into this command variable. */
 	unsigned char command = NULL;
 
+	/* Enter the task loop. */
 	while(1)
-    {
+    	{
+		/* 
+		 * Dequeue command sent from the interrupt service
+		 * routine.
+		 */
 		xQueueReceive(xQueue, &(command), portMAX_DELAY);
 
-		// Grab command data from PORTD;
-		// Save it in the shared memory;
-		// was 250
+		/* 
+		 * Write dequeued command into the shared memory variable
+		 * so that it is later read by the lightrope object.
+		 */
 		writeCom(command);
-		vTaskDelayUntil(&xLastWakeTime, (50 / portTICK_RATE_MS));
+		
+		/* Time delay before next loop iteration. */
+		vTaskDelayUntil(&xLastWakeTime, (100 / portTICK_RATE_MS));
 	}	
 }
 
+/*
+ * Initalise the telecommand object.
+ */
 void initTC()
 {
-	xQueue = xQueueCreate(3, sizeof(unsigned char));
+	/* Create the interrupt message relay queue. */
+	xQueue = xQueueCreate(1, sizeof(unsigned char));
+	
+	/* Create and run the telecommand task. */
 	xTaskCreate(listenTC, "listenTC", 300, NULL, 1, NULL);
 }
 
+/*
+ * Initalise the interrupt registry.
+ */
 void initInterrupt()
 {
-	// Use pull up as input, so don't set EICRA.
-	// We are going to use the external interrupts INT0
+	/* Use pull up as input, so no need to set EICRA. */
+	/* Using external interrupts INT0 as interrupt trigger */
 	EIMSK = (1 << INT0);
-	sei(); // activate interrupts
+	
+	/* Activate interrupts */
+	sei();
 }
 
-
-// Interrupt service routing for INT0
+/*
+ * Interrupt service routing for INT0.
+ */
 ISR(INT0_vect)
 {
-	//PORTB = 0b10101010;
+	/* Grap inverse value of PIND when interrupt trigger is fired. */
 	unsigned char button = ~PIND;
+	
+	/* And enqueue it in the command relay queue. */
 	xQueueSendToBackFromISR(xQueue, &button, NULL);
 }
-
-
-
-
-
